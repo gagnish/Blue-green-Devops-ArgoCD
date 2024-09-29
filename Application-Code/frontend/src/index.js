1541eecaefe84c073e9df28ad791970dcd08ff5a
gagnish <gdhiman@SEA.CORP.EXPECN.COM>
2024-09-29 23:16:21 +0530

    new adding

--- Application-Code/frontend/src/index.js
+++ Application-Code/frontend/src/index.js
@@ -1,12 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
 import "./index.css";
-import store from "./redux/store";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.render(
+    <React.StrictMode>
+        <div className="app-wrapper">
+            <App />
+        </div>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
+
