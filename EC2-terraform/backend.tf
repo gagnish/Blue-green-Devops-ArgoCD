ca9562e93bbb593c96de6a2dd941c8f49fc05522
gagnish <gdhiman@SEA.CORP.EXPECN.COM>
2024-09-29 14:30:38 +0530

    new adding

--- EC2-terraform/backend.tf
+++ EC2-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "blue-green-deployment-proj" # Replace with your actual S3 bucket name
+    bucket = "terrabucket22" # Replace with your actual S3 bucket name
     key    = "EC2/terraform.tfstate"
     region = "us-east-1"
   }
--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "blue-green-deployment-proj" # Replace with your actual S3 bucket name
+    bucket = "terrabucket22" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
